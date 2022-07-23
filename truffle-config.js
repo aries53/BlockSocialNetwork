be52f906e8a2096fff2f0595eb5f795e7c35a3aa
aries53 <glasses794@gmail.com>
2022-07-23 19:47:18 +0700

    Upload project

---
+++ truffle-config.js
@@ -0,0 +1,26 @@
+//require have accounts
+require('babel-register');
+//require accounts have money
+require('babel-polyfill');
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+  },
+  //Folder contracts 
+  contracts_directory: './src/contracts/',
+  //After Build contracts
+  contracts_build_directory: './src/abis/',
+  compilers: {
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
